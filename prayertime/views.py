3571832fcc71c4c7baeae499e2115d1aa1ac5e9f
doniyor <doniyorkarimoff96@gmail.com>
2022-02-11 18:33:25 +0500

    deployment settings

--- prayertime/views.py
+++ prayertime/views.py
@@ -8,7 +8,7 @@ import requests
 from django.core.files.storage import FileSystemStorage
 from django.core.files.base import ContentFile
 
-bot = telebot.TeleBot("5239654333:AAEH-PlBfxuLs51up6_35sntiU84KOIylpY", parse_mode="HTML")
+bot = telebot.TeleBot("5135451825:AAHgPN401uzsCHyaHcFihbLrrHH_Fij1kb0", parse_mode="HTML")
 
 
 @csrf_exempt
