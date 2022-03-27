f63f3979827e436e4ce9921fd5a710c19949eb6e
doniyor <doniyorkarimoff96@gmail.com>
2022-03-27 13:59:52 +0500

    deployment settings

--- prayertime/prayer.py
+++ prayertime/prayer.py
@@ -1,51 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
-from telebot import types
+import datetime
 
 
 def pray_time(a):
-    url1 = 'http://uzsmart.ru/namoz-vaqtlari/'
-    response1 = requests.get(url1)
-    soup1 = BeautifulSoup(response1.text, 'lxml')
-    current_time = soup1.find('span', id='current_time').text  # current time
-    date = soup1.find('h2', style='font-size: 24px; color: red').text[1:11]  # current date
-    print(soup1.find('div', class_='col-12 datatimes_prayer_title'))
+    while True:
+        print(datetime.datetime.now().strftime("%H:%M:%S"), end="\r")
+        # time.sleep(1)
+        now = datetime.datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        date = datetime.date.today()
+        url = f'https://islom.uz/vaqtlar/{a}/{datetime.datetime.now().month}'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        city_href = soup.find_all('tr', class_='p_day bugun')
 
-    url = f'https://islom.uz/vaqtlar/{a}/3'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    city_href = soup.find_all('tr', class_='p_day bugun')
-    # print(city_href)
-    for i in city_href:
-        table_data = i.find_all('td')
-        data = [j.text for j in table_data]
-        kun = data[2]
-        tong = data[3]
-        quyosh = data[4]
-        pewn = data[5]
-        asr = data[6]
-        shom = data[7]
-        xufton = data[8]
-        # print(kun, tong, quyosh, pewn, asr, shom, xufton, date, current_time)
-# pray_time(1)
+        for i in city_href:
+            table_data = i.find_all('td')
+            data = [j.text for j in table_data]
+            kun = data[2]
+            tong = data[3]
+            quyosh = data[4]
+            pewn = data[5]
+            asr = data[6]
+            shom = data[7]
+            xufton = data[8]
+            # print(kun, tong, quyosh, pewn, asr, shom, xufton, date, current_time)
 
-    dict = {"27": 'Тошкент', '37': 'Фарғона', '1': 'Андижон', '15': 'Наманган',
-            '4': "Бухоро", '9': 'Жиззах', '25': 'Қарши', '16': 'Нукус',
-            '14': 'Навоий', '18': 'Самарқанд', '21': 'Хива', '5': 'Гулистон', '6': 'Денов',
-            '26': 'Қўқон', '13': 'Марғилон', '3': 'Бишкек', '19': 'Туркистон',
-            '61': 'Зарафшон', '20': 'Ўш', '78': 'Урганч', '74': 'Термиз'}
-    if a in dict.keys():
-        y = dict[a]
+        # pray_time(1)
 
-    return f'⌛️<b>Намоз вақтлари <u><i>{y.upper()}</i></u> шаҳри бўйича:</b>\n' \
-           f'==============================\n' \
-           f'🏙<b>Тонг(Саҳарлик):</b>      《<i>{tong}</i>》\n' \
-           f'🌃<b>Куёш:</b>                         《<i>{quyosh}</i>》\n' \
-           f'-------------------------------------\n' \
-           f'🏙<b>Бомдод:</b>                    《<i>{tong}</i>》\n' \
-           f'🌇<b>Пешин:</b>                     《<i>{pewn}</i>》\n' \
-           f'🌆<b>Аср:</b>                            《<i>{asr}</i>》\n' \
-           f'🏙<b>Шом(Ифтор):</b>          《<i>{shom}</i>\n' \
-           f'🌃<b>Хуфтон:</b>                     《<i>{xufton}</i>》\n' \
-           f'==============================\n' \
-           f'📅 <u><b>Сана:</b> 《<i> {date}</i>》</u>  | <b><i>{kun}</i></b> | ⏱<u><b> Вақт:</b> 《<i> {current_time[0:5]}</i>》</u>'
+        dict = {"27": 'Тошкент', '37': 'Фарғона', '1': 'Андижон', '15': 'Наманган',
+                '4': "Бухоро", '9': 'Жиззах', '25': 'Қарши', '16': 'Нукус',
+                '14': 'Навоий', '18': 'Самарқанд', '21': 'Хива', '5': 'Гулистон', '6': 'Денов',
+                '26': 'Қўқон', '13': 'Марғилон', '3': 'Бишкек', '19': 'Туркистон',
+                '61': 'Зарафшон', '20': 'Ўш', '78': 'Урганч', '74': 'Термиз'}
+        if a in dict.keys():
+            y = dict[a]
+
+        return f'⌛️<b>Намоз вақтлари <u><i>{y.upper()}</i></u> шаҳри бўйича:</b>\n' \
+               f'==============================\n' \
+               f'🏙<b>Тонг(Саҳарлик):</b>      《<i>{tong}</i>》\n' \
+               f'🌃<b>Куёш:</b>                         《<i>{quyosh}</i>》\n' \
+               f'-------------------------------------\n' \
+               f'🏙<b>Бомдод:</b>                    《<i>{tong}</i>》\n' \
+               f'🌇<b>Пешин:</b>                     《<i>{pewn}</i>》\n' \
+               f'🌆<b>Аср:</b>                            《<i>{asr}</i>》\n' \
+               f'🏙<b>Шом(Ифтор):</b>          《<i>{shom}</i>\n' \
+               f'🌃<b>Хуфтон:</b>                     《<i>{xufton}</i>》\n' \
+               f'==============================\n' \
+               f'📅 <u><b>Сана:</b> 《<i> {date}</i>》</u>  | <b><i>{kun}</i></b> | ⏱<u><b> Вақт:</b> 《<i> {current_time}</i>》</u>'
